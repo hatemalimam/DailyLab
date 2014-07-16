988fc9d7a6ccd07dea812cb5d29dc440ea7dc750
hatemalimam <me@hatemalimam.com>
2014-07-16 11:40:22 +0200

    rating style

--- src/main/java/com/ha/dailylab/UserPojo.java
+++ src/main/java/com/ha/dailylab/UserPojo.java
@@ -6,6 +6,9 @@
 
 package com.ha.dailylab;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author hatemalimam
@@ -28,5 +31,24 @@ public class UserPojo {
 
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public static void main(String args[]) {
+        Double stash = 200d;
+        Double fooBarBasePrice = 1d;
+        List<Double> boughtBars = new ArrayList<Double>();
+        
+        while(stash > 0 && stash > fooBarBasePrice + (fooBarBasePrice * 20 / 100)) {
+            if(boughtBars.isEmpty()) {
+                boughtBars.add(fooBarBasePrice);
+                stash -= fooBarBasePrice;
+            } else {
+                fooBarBasePrice += fooBarBasePrice * 20 / 100;                
+                stash -= fooBarBasePrice;
+                boughtBars.add(fooBarBasePrice);
+            }             
+        }
+        System.out.println(stash);
+        System.out.println(boughtBars.size());
     }
 }
